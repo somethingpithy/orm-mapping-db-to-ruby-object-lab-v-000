730494ce75dd48c9efb6e2c7bbe8de0949d45da4
Carlota Bindner <cev.bindner@gmail.com>
2018-03-21 02:06:08 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,7 +71,15 @@ class Student
   end
 
   def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT name
+      FROM students
+      WHERE grade
+      BETWEEN 9 AND 11
 
+    SQL
+
+    DB[:conn].execute(sql)
   end
 
   def self.first_X_students_in_grade_10(x_students)
