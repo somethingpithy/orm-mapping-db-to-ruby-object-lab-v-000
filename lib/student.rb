c2cd45fc06794e3ea31c46073e4a19bcd76a91c3
Carlota Bindner <cev.bindner@gmail.com>
2018-03-21 02:17:18 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -106,8 +106,16 @@ class Student
     end.first
   end
 
-  def self.all_students_in_grade_X
+  def self.all_students_in_grade_X(grade_x)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+    SQL
 
+    DB[:conn].execute(sql, grade_x).map do |row|
+      self.new_from_db(row)
+    end
   end
 
 end
