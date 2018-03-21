58a83269346e3ef701b4d439438019fe1ab1b609
Carlota Bindner <cev.bindner@gmail.com>
2018-03-21 02:16:12 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -97,7 +97,8 @@ class Student
       SELECT *
       FROM students
       WHERE grade = 10
-      ORDER BY students.id LIMIT 1
+      ORDER BY students.id
+      LIMIT 1
     SQL
 
     DB[:conn].execute(sql).map do |row|
